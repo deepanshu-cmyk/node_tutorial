2a074229d1471f32b49f09bda7cf691c83267d9c
Deepanshu <deepanshu@webnexus.in>
2025-11-10 09:42:45 +0530

    server js make

--- node_tutorial/server.js
+++ node_tutorial/server.js
@@ -10,7 +10,7 @@ const server = http.createServer((req, res) => {
     }
 });
 
-const PORT = 3000
+const PORT = 4000
 
 server.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
